91b45bbf1287a33603c344d64c06b6b1bf8f226e
bumsoo-dmis <meliketoy@gmail.com>
2018-11-22 15:50:46 +0900

    inception & xception train script

---
+++ train/inception.sh
@@ -0,0 +1,8 @@
+python3 main.py \
+    --optimizer SGD \
+    --lr 0.045 \
+    --weight_decay 4e-5 \
+    --net_type inception \
+    --depth 50 \
+    --resetClassifier \
+    --finetune
---
+++ train/xception.sh
@@ -0,0 +1,8 @@
+python3 main.py \
+    --lr 0.045 \
+    --optimizer SGD \
+    --weight_decay 1e-5 \
+    --net_type xception \
+    --resetClassifier \
+    --finetune
+    #--testOnly
